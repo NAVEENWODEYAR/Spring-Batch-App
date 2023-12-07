5b50d0df05f03620076adb423eb6ce2b2f168b20
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-07 17:59:45 +0530

    initial run

--- src/main/java/com/bhas/BatchProcessingAppApplication.java
+++ src/main/java/com/bhas/BatchProcessingAppApplication.java
@@ -1,13 +1,22 @@
 package com.bhas;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class BatchProcessingAppApplication {
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan
+public class BatchProcessingAppApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(BatchProcessingAppApplication.class, args);
+		System.out.println("\n @SpringBootConfiguration,@ComponentScan,@EnableAutoConfiguration");
 	}
 
 }
